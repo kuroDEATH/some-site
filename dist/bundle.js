4b8f8d08259e1a7c43dfb2f6cccc1ccc4d6b7e08
kuroDEATH <deathvg@gmail.com>
2022-05-08 15:37:45 +0300

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,17 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+
+const burger = document.querySelector('.header__burger');
+const list = document.querySelector('.header__list');
+
+burger.addEventListener('click', () => {
+    console.log('object');
+    burger.classList.toggle('open-menu');
+    list.classList.toggle('open-menu');
+    document.body.classList.toggle('fixed-page');
+});
+/******/ })()
+;
